c3f63d9d63f8949a5a2c3cdf72256ed12ce924c3
Maika Kailani <queenmab051@gmail.com>
2019-11-24 20:01:55 +0000

    Done.

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,22 @@
 def oxford_comma(array)
-
+  array_size = array.length
+  if array_size == 2 
+    return array.join(" and ")
+  elsif array_size == 1
+    return array[0]
+  else
+    number = 0
+    sentence = ""
+    array.each do |fruit|
+      if number == array_size - 2
+        sentence << fruit + ", and "
+      elsif number == array_size - 1 
+        return sentence  << fruit
+      else
+        sentence << fruit + ", "
+      end
+      number += 1
+    end
+    return sentence
+  end
 end
